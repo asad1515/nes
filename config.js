369bc80be55467beb79e621031acdfa74157c74d
asad1515 <47108391+asad1515@users.noreply.github.com>
2020-04-29 09:58:41 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  "token": "NjY4OTM3MzYyNDExMDk0MDQx.XiYjpA.DBtKId8Y9NMPnXBvD_5IIDXEFV4",
+  "sahip": "324201796945838091",
+
+  "dashboard" : {
+    "oauthSecret": "jGEroIYayQ-qSGt2_WORk-O6A3GLCQ2b", // This is the `client` secret in your bot application page.
+    "callbackURL": "https://flexbotofficial.glitch.me/callback",
+    "sessionSecret": "Flexbot Dashboard",
+    "domain": "https://flexbotofficial.glitch.me",
+    "port": 8000
+  },
+};
+
+module.exports = config;
